09bfd3d248b8aa7f8ac065578f4506fd34a76462
JoenTheWizard <joenwiz@localhost.localdomain>
2024-07-01 00:32:18 +1000

    add readme file, update comments in perlin.h, add image source

---
+++ README.md
@@ -0,0 +1,3 @@
+# raylib game
+
+Simple raylib game made in C++. It's not feature complete but can serve as a basic template. It generates terrain using 2D Perlin Noise and uses UDP packets for simple and basic multiplayer functionality. 
--- includes/perlin.h
+++ includes/perlin.h
@@ -1,6 +1,8 @@
 #ifndef PERLIN_H
 #define PERLIN_H
 
+//Credit goes to https://gist.github.com/nowl/828013?permalink_comment_id=2807232#gistcomment-2807232 for the 2D Perlin Implementation
+
 //2D based perlin noise
 extern double perlin2d(double x, double y, double freq, int depth);
 
